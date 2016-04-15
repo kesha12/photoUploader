1c249290da8dcfb8e74099ff51592356137b0fbe
Rituja Kesharwani <ritujakesharwani111@gmail.com>
2016-04-15 12:33:51 +0530

    Update photo.rb

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
 	has_attached_file :image, preserve_files: "true",
-                   styles: { thumb: ["100#x100#", :jpg],
+                   styles: { thumb: ["100x100#", :jpg],
                             original: ['500x500>', :jpg] },
                             convert_options: { thumb: "-quality 75 -strip",
                                      original: "-quality 85 -strip" },
